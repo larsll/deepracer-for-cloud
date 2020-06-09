4ccc7bd92ffa50bb5ec0d72158943a6747b67f31
dartjason <51768630+dartjason@users.noreply.github.com>
2020-06-08 23:29:52 -0400

    Update autorun.sh

--- bin/autorun.sh
+++ bin/autorun.sh
@@ -32,6 +32,6 @@ dr-update
 date | tee $INSTALL_DIR_TEMP/DONE-AUTORUN
 
 ## start training -- commented out for now until I can figure out how to do it properly
-cd /home/ubuntu/deepracer-for-cloud/scripts/training 
+cd $INSTALL_DIR_TEMP/scripts/training 
 nohup ./start.sh &
 
