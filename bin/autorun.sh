8673c7269cdaff326480ecce8f2a79ccb99942dd
dartjason <51768630+dartjason@users.noreply.github.com>
2020-06-12 22:07:11 -0400

    Update autorun.sh

--- bin/autorun.sh
+++ bin/autorun.sh
@@ -7,7 +7,7 @@
 INSTALL_DIR_TEMP="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
 ## retrieve the s3_location name you sent the instance in user data launch
-S3_LOCATION=$(cat $INSTALL_DIR_TEMP/tmp/s3_training_location.txt)
+S3_LOCATION=$(cat $INSTALL_DIR_TEMP/bin/s3_training_location.txt)
 
 ## get the updatated run.env and system.env files you created and stashed in s3
 aws s3 cp s3://$S3_LOCATION/run.env $INSTALL_DIR_TEMP/run.env
