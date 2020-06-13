f8e03f503b2384452477b85c17e2a05e134e8a51
dartjason <51768630+dartjason@users.noreply.github.com>
2020-06-12 21:19:45 -0400

    Update sample-createspot.sh

--- utils/sample-createspot.sh
+++ utils/sample-createspot.sh
@@ -3,17 +3,16 @@
 ##  This is sample code that will generally show you how to launch a spot instance on aws and leverage the 
 ##  automation built into deepracer-for-cloud to automatically start training
 ##  Changes required to work:
-##     Input bucket info
+##     Input location where your training will take place -- S3_LOCATION
 ##     Input security group, iam role, and key-name
 
-## BUCKET is the fundamental input. you need to tell it where in s3 your training will be stored
-## just the bucket name.  no S3:// etc
+## First you need to tell the script where in s3 your training will take place
+## can be either a bucket, or a bucket/prefix
 
-BUCKET=<########>
+S3_LOCATION=<#########>
 
 
 ## Fill these out with your custom information if you want to upload and submit to leaderboard.  not required to run
-DR_UPLOAD_S3_BUCKET=########
 DR_UPLOAD_S3_PREFIX=########
 
 ## set the instance type you want to launch
@@ -28,14 +27,15 @@ DR_WORKERS=1
 
 ## select which images you want to use.  these will be used later for a docker pull
 DR_SAGEMAKER_IMAGE=cpu-avx-mkl
-DR_ROBOMAKER_IMAGE=2.0.7-cpu-avx2
+DR_ROBOMAKER_IMAGE=cpu-avx2
 
 
-## check the s3 bucket location for existing training folders
+## check the s3 location for existing training folders
 ## automatically determine the latest training run (highest number), and set model parameters accordingly
 ## this script assumes the format rl-deepracer-1, rl-deepracer-2, etc.  modify here if your schema differs
 
-LAST_TRAINING=$(aws s3 ls $BUCKET/rl-deepracer | sort -t - -k 3 -g | tail -n 1 | awk '{print $2}')
+
+LAST_TRAINING=$(aws s3 ls $S3_LOCATION/rl-deepracer | sort -t - -k 3 -g | tail -n 1 | awk '{print $2}')
 ## drop trailing slash
 LAST_TRAINING=$(echo $LAST_TRAINING | sed 's:/*$::')
 
@@ -74,23 +74,23 @@ sed -i.bak -re "s/(DR_WORLD_NAME=).*$/\1$WORLD_NAME/g" "$OLD_RUNENV"
 sed -i.bak -re "s/(DR_UPLOAD_S3_BUCKET=).*$/\1$DR_UPLOAD_S3_BUCKET/g; s/(DR_SAGEMAKER_IMAGE=).*$/\1$DR_SAGEMAKER_IMAGE/g; s/(DR_ROBOMAKER_IMAGE=).*$/\1$DR_ROBOMAKER_IMAGE/g; s/(DR_WORKERS=).*$/\1$DR_WORKERS/g" "$OLD_SYSTEMENV"
 
 
-## upload the new run.env and system.env files into your S3 bucket (same bucket identified earlier)
-aws s3 cp ./run.env s3://$BUCKET/run.env
-aws s3 cp ./system.env s3://$BUCKET/system.env
+## upload the new run.env and system.env files into your S3 location (same location identified at beginning of script)
+aws s3 cp ./run.env s3://$S3_LOCATION/run.env
+aws s3 cp ./system.env s3://$S3_LOCATION/system.env
 
 ## upload a custom autorun script to S3.  there is a default autorun script in the repo that will be used unless a custom one is specified here instead
-#aws s3 cp ./autorun.sh s3://$BUCKET/autorun.sh
+#aws s3 cp ./autorun.sh s3://$S3_LOCATION/autorun.sh
 
 ## upload custom files -- if you dont want this, comment these lines out
-aws s3 cp ./model_metadata.json s3://$BUCKET/custom_files/model_metadata.json
-aws s3 cp ./reward_function.py s3://$BUCKET/custom_files/reward_function.py
-aws s3 cp ./hyperparameters.json s3://$BUCKET/custom_files/hyperparameters.json
+aws s3 cp ./model_metadata.json s3://$S3_LOCATION/custom_files/model_metadata.json
+aws s3 cp ./reward_function.py s3://$S3_LOCATION/custom_files/reward_function.py
+aws s3 cp ./hyperparameters.json s3://$S3_LOCATION/custom_files/hyperparameters.json
 
 
 ## launch an ec2
 ## update with your own settings, including key-name, security-group, and iam-instance-profile at a minimum
-## user data includes a command to create a bucket.txt file which simply contains the name of the bucket
-## this bucket name will be used as fundamental input to autorun.sh script run later on that instance
+## user data includes a command to create a .txt file which simply contains the name of the s3 location
+## this filename will be used as fundamental input to autorun.sh script run later on that instance
 ## you need to ensure you have proper IAM permissions to launch this instance
 
 aws ec2 run-instances \
@@ -103,4 +103,4 @@ aws ec2 run-instances \
     --iam-instance-profile Arn=arn:aws:iam::<####acct_num####>:instance-profile/<####role_name####> \
     --instance-market-options MarketType=spot \
     --user-data "#!/bin/bash
-    su -c 'git clone https://github.com/larsll/deepracer-for-cloud.git && echo "$BUCKET" > /home/ubuntu/deepracer-for-cloud/bucket.txt && /home/ubuntu/deepracer-for-cloud/bin/prepare.sh' - ubuntu"
+    su -c 'git clone https://github.com/larsll/deepracer-for-cloud.git && echo "$S3_LOCATION" > /home/ubuntu/deepracer-for-cloud/bucket.txt && /home/ubuntu/deepracer-for-cloud/bin/prepare.sh' - ubuntu"
