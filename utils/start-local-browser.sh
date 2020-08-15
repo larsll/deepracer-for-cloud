b06424f351f886e25d1b5187a8519273dffed1f6
dungviettran89 <dungviettran89@gmail.com>
2020-08-15 20:20:24 +0800

    Allow viewing multiple streams in swarm mode

--- utils/start-local-browser.sh
+++ utils/start-local-browser.sh
@@ -43,14 +43,16 @@ esac
 done
 
 FILE=$DR_DIR/tmp/streams-$DR_RUN_ID.html
+NGINX=$DR_DIR/tmp/streams-$DR_RUN_ID.conf
 
-# Check if we will use Docker Swarm or Docker Compose
-if [[ "${DR_DOCKER_STYLE,,}" == "swarm" ]];
-then
-  echo "This script does not support swarm mode."
-  exit
-fi
-
+cat << EOF > $NGINX
+server {
+  listen 80;
+  location / {
+    root   /usr/share/nginx/html;
+    index  index.html index.htm;
+  }
+EOF
 echo "<html><head><title>DR-$DR_RUN_ID - $DR_LOCAL_S3_MODEL_PREFIX - $TOPIC</title></head><body><h1>DR-$DR_RUN_ID - $DR_LOCAL_S3_MODEL_PREFIX - $TOPIC</h1>" > $FILE
 
 ROBOMAKER_CONTAINERS=$(docker ps --format "{{.ID}}" --filter name=deepracer-$DR_RUN_ID --filter "ancestor=awsdeepracercommunity/deepracer-robomaker:$DR_ROBOMAKER_IMAGE")
@@ -60,12 +62,21 @@ if [ -z "$ROBOMAKER_CONTAINERS" ]; then
 fi
 
 for c in $ROBOMAKER_CONTAINERS; do
-    C_PORT=$(docker inspect $c | jq -r '.[0].NetworkSettings.Ports["8080/tcp"][0].HostPort')
-    C_URL="http://localhost:${C_PORT}/stream?topic=${TOPIC}&quality=${QUALITY}&width=${WIDTH}&height=${HEIGHT}"
+    C_URL="/$c/stream?topic=${TOPIC}&quality=${QUALITY}&width=${WIDTH}&height=${HEIGHT}"
     C_IMG="<img src=\"${C_URL}\"></img>"
     echo $C_IMG >> $FILE
+    echo "  location /$c { proxy_pass http://$c:8080; rewrite /$c/(.*) /\$1 break; }" >> $NGINX
 done
 
 echo "</body></html>" >> $FILE
+echo "}" >> $NGINX
+docker rm -f deepracer-$DR_RUN_ID-viewer
+docker run -d \
+  -p 8081:80 \
+  --network sagemaker-local \
+  --name deepracer-$DR_RUN_ID-viewer \
+  -v $FILE/:/usr/share/nginx/html/index.html \
+  -v $NGINX:/etc/nginx/conf.d/default.conf \
+  nginx
 echo "Starting browser '$BROWSER'."
-$BROWSER `readlink -f $FILE ` &
+$BROWSER "http://127.0.01:8081" &
